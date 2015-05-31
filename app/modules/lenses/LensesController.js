366f234cbad8d4835d888fadd1c7f63fca57533b
Brandon Parmenter <leftiness@gmail.com>
2015-05-30 20:43:32 -0400

    Added buttons
    - Add, edit, delete, clear, toggle knobs
    - Add button doubles as a toggle for other buttons

--- app/modules/lenses/LensesController.js
+++ app/modules/lenses/LensesController.js
@@ -1,7 +1,49 @@
-function LensesCtrl($scope, $stateParams, Restangular, CONSTANTS) {
+function LensesCtrl($scope, $stateParams, $mdMedia, Restangular, CONSTANTS) {
 	'use strict';
+
+	var _ = CONSTANTS.lodash;
 	$scope.params = $stateParams;
 	$scope.query = {};
+
+	$scope.showKnobs = function () {
+		return _.isUndefined(_.get($scope, 'show.knobs')) ?
+			$mdMedia('gt-lg') :
+			$scope.show.knobs;
+	};
+
+	$scope.toggleKnobs = function () {
+		$scope.showKnobs() ?
+		_.set($scope, 'show.knobs', false) :
+		_.set($scope, 'show.knobs', true);
+	};
+
+	$scope.countSelected = function () {
+		var count = _.countBy($scope.records, 'isSelected')[true];
+		return _.isUndefined(count) ? 0 : count;
+	};
+
+	$scope.delete = function () {
+		$scope.records = _.reject($scope.records, {'isSelected': true});
+		// TODO: Implement REST
+	};
+
+	$scope.edit = function () {
+		// TODO: Implement state change
+	};
+
+	$scope.add = function () {
+		var show = _.get($scope, 'show.buttons');
+		if (_.isUndefined(show) || show === true) {
+			// TODO: Implement state change
+		}
+	};
+
+	$scope.clear = function () {
+		_.forEach($scope.records, function (record) {
+			delete record.isSelected;
+		});
+	};
+
 	Restangular.one('lenses', $stateParams.id).getList('records').then(
 		function (records) {
 			$scope.safeRecords = records;
@@ -15,14 +57,15 @@ function LensesCtrl($scope, $stateParams, Restangular, CONSTANTS) {
 
 	Restangular.one('lenses', $stateParams.id).getList().then(
 		function (knobs) {
-			$scope.knobs = CONSTANTS.lodash.sortBy(knobs, 'order');
+			$scope.knobs = _.sortBy(knobs, 'order');
 		},
 		function () {
 			$scope.knobs = [];
 		}
 	);
+
 }
 
-LensesCtrl.$inject = ['$scope', '$stateParams', 'Restangular', 'CONSTANTS'];
+LensesCtrl.$inject = ['$scope', '$stateParams', '$mdMedia', 'Restangular', 'CONSTANTS'];
 
 module.exports = LensesCtrl;
