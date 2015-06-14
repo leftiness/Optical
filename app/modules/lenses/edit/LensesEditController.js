cb8e2965677193d587fc0f0e50045f2c8ebc7549
Brandon Parmenter <leftiness@gmail.com>
2015-06-14 19:42:17 -0400

    Added some real UI to the edit page

--- app/modules/lenses/edit/LensesEditController.js
+++ app/modules/lenses/edit/LensesEditController.js
@@ -1,10 +1,33 @@
-function LensesEditCtrl() {
+function LensesEditCtrl(LensesService, $stateParams) {
 	'use strict';
 
 	var self = this;
 
+	this.params = $stateParams;
+	this.record = {};
+	this.knobs = [];
+
+	// TODO: Inherit knobs as a child state?
+
+	/*
+	 * TODO: Pass record as a parameter somehow instead of downloading it again
+	 * with LensesService? Maybe cache it on the parent controller. If the cache
+	 * has a record with this ID, use that one. Otherwise, download it again.
+	 */
+
+	(function init() {
+		var lens = self.params.lens;
+		var record = self.params.record;
+		LensesService.getRecord(lens, record).then(function (record) {
+			self.record = record;
+		});
+		LensesService.getAllKnobs(lens).then(function (knobs) {
+			self.knobs = knobs;
+		});
+	})();
+
 }
 
-LensesEditCtrl.$inject = [];
+LensesEditCtrl.$inject = ['LensesService', '$stateParams'];
 
 module.exports = LensesEditCtrl;
